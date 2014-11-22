322e1876c90503a15fadd704a2709f20717fa96a
Brett Jones <brettjonesdev@gmail.com>
2014-11-22 15:02:13 -0600

    create output files

--- start.js
+++ start.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var _ = require('underscore');
 var Handlebars = require('handlebars');
 var moment = require('moment');
+var mkdirp = require('mkdirp');
 require('./helpers');
 
 var GAME_STATUS_FINAL = 3;
@@ -21,11 +22,18 @@ nba.ready(function() {
     var team = _.find(nba.teamsInfo, function(team) {
         return team.simpleName.toUpperCase() == teamName.toUpperCase()
     });
-    console.log("Looking up last game for team", team, team.teamId);
+    console.log("Looking up last game for ", team.teamName);
+    global.team = team;
 
     var date = argLength > 3 ? moment(process.argv[3]).toDate() : new Date();
 
-    getLastGameForTeam(team.teamId, date, outputFourFactors);
+    getLastGameForTeam(team.teamId, date, function(game) {
+
+        nba.api.boxScoreFourFactors({gameId: game.gameId})
+            .then(outputFourFactors);
+
+
+    });
 });
 
 function getLastGameForTeam(teamId, date, callback) {
@@ -41,6 +49,7 @@ function getLastGameForTeam(teamId, date, callback) {
             date = moment(date).subtract(1, 'days').toDate();
             getLastGameForTeam(teamId, date, callback);
         } else {
+            global.date = date;
             callback(teamGame);
         }
     });
@@ -48,14 +57,23 @@ function getLastGameForTeam(teamId, date, callback) {
 }
 
 
-function outputFourFactors(game) {
-    console.log(game);
-    nba.api.boxScoreFourFactors({gameId: game.gameId}).then(function(resp) {
-        console.log(resp);
-        var template = Handlebars.compile(fs.readFileSync('./templates/fourFactors.hbs', "utf8"));
-        var data = resp.sqlTeamsFourFactors[0];
-        data.opponentName = resp.sqlTeamsFourFactors[1].teamName;
-        var html = template(resp.sqlTeamsFourFactors[0]);
-        console.log(html);
-    })
+function outputFourFactors(resp) {
+    var template = Handlebars.compile(fs.readFileSync('./templates/fourFactors.hbs', "utf8"));
+    var fourFactors = resp.sqlTeamsFourFactors;
+
+    console.log("our team", global.team);
+    var ourTeam = _.findWhere(fourFactors, {teamId: global.team.teamId});
+    var theirTeam = _.without(fourFactors, ourTeam)[0];
+    var data = ourTeam;
+    data.opponentName = theirTeam.teamName;
+    console.log(data);
+    var html = template(data);
+    console.log("html", html);
+    mkdirp(getDirectoryName(), function(err) {
+        fs.writeFile(getDirectoryName() + 'fourFactors.html', html);
+    });
+}
+
+function getDirectoryName() {
+    return './output/' + global.team.teamName + '/' + moment(global.date).format('MM-DD-YYYY') + '/';
 }
